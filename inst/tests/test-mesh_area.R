885407e056892e736477c71b4d4c5991974dd463
Cornel M. Pop <pop.cornelm@gmail.com>
2018-09-10 22:06:36 -0700

    Added pre-existing test and consolidated tests for this functionality changing context accordingly.

--- inst/tests/test-mesh_area.R
+++ inst/tests/test-mesh_area.R
@@ -1,4 +1,4 @@
-context("Mesh attributes")
+context("Mesh area")
 
 test_that("getTArea produces correct output", {
   validTriangle1 <- c(1, 1, 0, 1, 5, 0, 10, 1, 0)
@@ -16,4 +16,18 @@ test_that("getTArea produces correct output", {
   # Bad input = NA
   expect_that(is.na(Lithics3D:::getTArea(invalidTriangle2)), is_true())
   expect_that(Lithics3D:::getTArea(degenTriangle), equals(0))
+})
+
+test_that("mesh_area produces correct output", {
+  data(demoSurface)
+
+  # Note: Will have to increase the precision of the computations... not sure
+  # why it's not higher.
+  expect_that(round(mesh_area(demoSurface), 4), equals(278.5047))
+  expect_that(round(mesh_area(demoSurface, par_exec = TRUE, max_cores = 2), 4),
+              equals(278.5047))
+
+  # Note: Catching errors at the level of individual triangles is the
+  # responsibility of the getTArea function.
+  expect_error(mesh_area("23"))
 })
