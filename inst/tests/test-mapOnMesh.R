d4465b96a53772a207ca03da37d844d4ca8439cd
Cornel M. Pop <pop.cornelm@gmail.com>
2018-09-10 22:25:25 -0700

    Cleaned and re-added pre-existing tests

---
+++ inst/tests/test-mapOnMesh.R
@@ -0,0 +1,31 @@
+context("Mesh mapping")
+
+test_that("mapOnMesh produces correct output", {
+  data(demoSurface)
+
+  goodDF <- data.frame(x = c(1, 2, 3), y = c(1, 2, 3), z = c(1, 2, 3))
+  badDF <- data.frame(x = c(NA, 2, 3), y = c(1, 2, 3), z = c(1, NaN, 3))
+  badDF2 <- data.frame(x = c("1", 2, 3), y = c("a", 2, 3), z = c(1, 2, 3))
+
+  ## Check for coord input handling.
+  # Requires matrix/dataframe of coords.
+  expect_error(mapOnMesh(c(1, 2, 3), demoSurface))
+  # Proper handling of bad coord input.
+  expect_warning(mapOnMesh(badDF, demoSurface))
+  # Proper handling of bad coord input.
+  expect_warning(mapOnMesh(badDF2, demoSurface))
+
+  # Check for mesh input handling.
+  expect_error(mapOnMesh(goodDF, list(vb = c("a", "b", "c")))) # Not a mesh!
+
+  # Check for correct output:
+  queryPts <- data.frame(x = c(-1, -1, 17),
+                       y = c(-2, 5, 10),
+                       z = c(1, 1, 1))
+  res <- mapOnMesh(queryPts, demoSurface)
+
+  # Note: to check how this works, do a plot(t(demoSurface$vb)[,1:2])
+  expect_that(as.numeric(res[1, 1:2]), equals(c(0, 0)))
+  expect_that(as.numeric(res[2, 1:2]), equals(c(0, 5)))
+  expect_that(as.numeric(res[3, 1:2]), equals(c(16, 10)))
+})
