a4f39c96293bd9a9e8cd88397c940e61595360b4
Cornel M. Pop <pop.cornelm@gmail.com>
2023-11-03 14:13:47 -0700

    Updated align_contour.R:
    1) added temporary code to fix a small, negligible-impact off-by-one issue with the output
    
    2) clarified function documentation
    
    3) Enhanced example (now it works on a centered demo flake)

--- R/align_contour.R
+++ R/align_contour.R
@@ -1,11 +1,13 @@
-#' @title Orient a mesh using a countour
-#' @description Re-orients a mesh and a specified contour running along the mesh
-#' surface so that the contour is as well aligned to the xy axes as possible.
-#' This is achieved by first applying a PCA on the input contour and then
-#' rotating the mesh using the same rotation matrix.
+#' @title Orient a mesh using a contour
+#' @description Re-orients a mesh and a specified contour so that the contour is
+#' as well aligned to the xy axes as possible. This is achieved by first
+#' applying a PCA on the input contour and then rotating the mesh using the same
+#' rotation matrix.
 #' 
-#' This may be useful if there is a need to orient a flake (or a specific
-#' surface component, such as the ventral part), using only edge landmarks.
+#' This may be useful if there is a need to orient a flake, for instance, or a
+#' specific surface component, such as the ventral part, using only edge
+#' landmarks.
+#' 
 #' @author Cornel M. Pop
 #' @param coords.o A Nx3 matrix-like object containing ordered xyz coordinates
 #' defining a closed contour, one coordinate per row.
@@ -14,18 +16,29 @@
 #' default is 100.
 #' @param mesh An object of class mesh3d to reorient.             
 #' @return A list containing the re-oriented mesh (mesh), the re-oriented input
-#'         contour (contour), the re-oriented resampled contour (contour.res),
+#'         contour (contour), the re-oriented re-sampled contour (contour.res),
 #'         and the PCA-derived rotation matrix (rot.mx) used for the
 #'         reorientation.
 #' @examples
 #' library(rgl)
 #' data(demoFlake2)
-#' m.rot = mesh_orient_by_contour_pca(demoFlake2$lms, demoFlake2$mesh, npts=100)
+#' m.rec <- mesh_recenter(demoFlake2$mesh, demoFlake2$lms)
+#' m.rot = mesh_orient_by_contour_pca(m.rec$coords, m.rec$mesh, npts=100)
 #' view3d(theta=0, phi=0)
 #' shade3d(m.rot$mesh, color="green")
-#' @section TODO: Add tests
+#' @note KNOWN ISSUES
+#' 
+#' - The re-sampled contour points in the output are not guaranteed to be on the
+#' mesh surface. In fact, they most likely won't be; if you want to ensure that
+#' they are, create a surface path first (see the sPathConnect function), and
+#' re-sample that.
+#' 
+#' @seealso sPathConnect, orient_by_vectors
 #' @export
 mesh_orient_by_contour_pca <- function(coords.o, mesh, npts=100){
+  
+  npts = npts - 1 # FIXME: digit.curves output is off-by-one. Shouldn't matter.
+
   # Resample contour to the desired npts
   contour.res <- digit.curves.old(start = coords.o[1, ],
                                   curve = coords.o[1:nrow(coords.o), ],
