30083f95467af71e9668bb3229a4ff0276f95c8f
Cornel M. Pop <pop.cornelm@gmail.com>
2023-11-03 15:20:19 -0700

    Slight improvements tot he documentation of the mesh_recenter function.

--- R/mesh_recenter.R
+++ R/mesh_recenter.R
@@ -1,32 +1,42 @@
 #' @title Recenter mesh coordinates
-#' @description Transforms a mesh so that its center (i.e., the average x, y,
-#' and z vertex coordinates) is at x=0, y=0, and z=0. The same transformation
-#' can be applied to coordinates supplied via an optional argument, so that
-#' the spatial relationships between the mesh and these coordinates can be
-#' preserved.
-#' @author Cornel M. Pop
-#' @param mesh An object of class mesh3d.
+#' @description Transforms a mesh so that its center is at the origin (0, 0, 0);
+#' it does so by subtracting the average x, y, and z coordinates from all
+#' vertices. The same transformation can be applied to the optional set of
+#' coordinates to preserve spatial relationships between the mesh and these
+#' coordinates.
+#'
+#' @param mesh An object of class \code{mesh3d} representing the 3D mesh.
 #' @param coords (Optional) An Nx3 matrix-like object containing xyz
-#' coordinates, one coordinate per row.
-#' @return A list containing the translated mesh (mesh) and coordinates (coords)
+#' coordinates, one coordinate per row. Default is \code{NA}.
+#' @return A list containing the translated mesh (\code{mesh}) and coordinates
+#' (\code{coords})
+#'
 #' @examples
+#' \dontrun{
 #' library(rgl)
 #' data(demoFlake2)
 #' res <- mesh_recenter(demoFlake2$mesh, demoFlake2$lms)
 #' view3d(theta=0, phi=0)
 #' shade3d(res$mesh, color="green")
 #' points3d(res$coords, color="red", size=5)
+#' }
+#'
+#' @author Cornel M. Pop
+#'
 #' @section TODO: Add tests
 #' @export
 mesh_recenter <- function(mesh, coords = NA) {
+  # Calculate the center of the mesh
   mesh_c <- c(mean(mesh$vb[1, ]),
               mean(mesh$vb[2, ]),
               mean(mesh$vb[3, ]))
 
+  # Recenter the mesh vertices
   mesh$vb[1, ] <- mesh$vb[1, ] - mesh_c[1]
   mesh$vb[2, ] <- mesh$vb[2, ] - mesh_c[2]
   mesh$vb[3, ] <- mesh$vb[3, ] - mesh_c[3]
 
+  # Recenter the optional coordinates, if provided
   if (!missing(coords)) {
     coords[, 1] <- coords[, 1] - mesh_c[1]
     coords[, 2] <- coords[, 2] - mesh_c[2]
