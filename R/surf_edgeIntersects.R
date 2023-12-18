cca3e71ae57636b8d24340ad5426c6f39d007af4
Cornel M. Pop <pop.cornelm@gmail.com>
2023-12-18 14:39:33 -0800

    e2sIntersect: Changed no-intersection output from NA to an empty dataframe so that the output is of the same type under all circumstances, and modified the flow slightly to improve performance. Also updated documentation and linted code.

--- R/surf_edgeIntersects.R
+++ R/surf_edgeIntersects.R
@@ -24,65 +24,104 @@ edgesOnPlane <- function(p, mpts, medges){
 }
 
 #' @title Mesh edge / sphere intersection
-#' @description Computes the intersection coordinates of the input mesh edges
-#' and a sphere.
-#' @param e.ids A vector containing edge IDs as output by Rvcg
-#' vcgGetEdge function with "unique" set to TRUE.
+#' @description
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' Computes the intersection coordinates of mesh edges (i.e., the sides of the
+#' triangles that form the triangular mesh) and a sphere for cases where edge
+#' ends are on opposite sides of the sphere's surface (i.e., one end is within
+#' the sphere, the other end is outside of it). In this implementation no
+#' intersections are returned for edges that cut across the sphere.
+#'
+#' @param e_ids A vector containing mesh edge IDs, corresponding to the row
+#' names of the [Rvcg::vcgGetEdge] output when run with "unique" set to TRUE).
+#'
 #' @param s A vector of length 4 (center.x, center.y, center.z, radius) defining
 #' a sphere
-#' @param mpts Nx4 matrix-like object corresponding to the transposed mesh$vb
-#' @param medges An Nx4 dataframe corresponding to the output of
-#' Rvcg::vcgGetEdge(mesh, unique=T).
+#'
+#' @param mpts Nx4 matrix-like object corresponding to the transposed mesh
+#' coordinates (`t(mesh$vb)`, where `mesh` is a `mesh3d` object)
+#'
+#' @param medges An Nx4 data.frame corresponding to the output of
+#' `Rvcg::vcgGetEdge(mesh, unique=T)`, where `mesh` is a `mesh3d` object.
+#' May be identical to the e_ids parameter, or the latter may be subset.
+#'
 #' @return A data.frame with intersection coordinates (x,y,z), one per
-#' row, for all input edges, where the row names correspond to input edge ids.
-#' NA will be returned in cases where there are no intersections. The first
-#' coordinate (x,y,z) will always be located ON the segment and farthest
-#' from the center of the sphere (i.e. if the segment crosses the entire
-#' sphere).
+#' row, for all input edges; the row names correspond to input edge ids.
+#'
 #' @examples
-#' print("TODO")
-#' @section TODO: Test this function with 3 different scenarios: no inters,
-#' inters, and both inters on segment.
+#' data(demoSphere) # Use the included demoSphere object for this example
+#' t_edges <- Rvcg::vcgGetEdge(demoSphere, unique = TRUE)
+#' t_sphere <- c(0.5, 0.5, 0.5, 1) # Offset by 0.5 from demoSphere's center
+#'
+#' # Compute intersections (if any) with all edges of the demoSphere:
+#' res <- e2sIntersect(rownames(t_edges),
+#'                     t_sphere,
+#'                     t(demoSphere$vb), # transpose vertex coordinates
+#'                     t_edges)
+#' \dontrun{
+#' wire3d(demoSphere, col = "black")
+#' spheres3d(t_sphere[1:3], col = "green", alpha = 0.5)
+#' points3d(res, col = "red")
+#'
+#' # Close the 3D window:
+#' close3d()
+#'}
+#'
+#' @author Cornel M. Pop
 #' @export
-e2sIntersect <- function(e.ids, s, mpts, medges){
+e2sIntersect <- function(e_ids, s, mpts, medges) {
+
+  # Basic input checking:
+  #stopifnot(is.atomic(e_ids))
+  #stopifnot(is.atomic(s) & length(s) == 4)
+  #stopifnot(length(dim(mpts)) == 2 & dim(mpts)[2] == 4)
+  #stopifnot(length(dim(medges)) == 2 & dim(medges)[2] == 4)
+
   d <- s[4]
 
-  vb1 <- mpts[medges[e.ids, 1], 1:3] # Coords of first edge end
-  vb2 <- mpts[medges[e.ids, 2], 1:3] # Coords of second edge end
+  # Set output specs here in case of multiple returns.
+  out_colnames <- c("x", "y", "z")
+  empty_out <- data.frame(matrix(nrow = 0, ncol = 3))
+  colnames(empty_out) <- out_colnames
+
+  vb1 <- mpts[medges[e_ids, 1], 1:3, drop = FALSE] # Coords of first edge end
+  vb2 <- mpts[medges[e_ids, 2], 1:3, drop = FALSE] # Coords of second edge end
 
   # Distances for the first (vb1) and second (vb2) ends
-  vb1.d <- sqrt( (vb1[, 1] - s[1]) ^ 2 + (vb1[, 2] - s[2]) ^ 2 +
-                 (vb1[, 3] - s[3]) ^ 2)
-  vb2.d <- sqrt( (vb2[, 1] - s[1]) ^ 2 + (vb2[, 2] - s[2]) ^ 2 +
-                 (vb2[, 3] - s[3]) ^ 2)
+  vb1_d <- sqrt((vb1[, 1] - s[1]) ^ 2 + (vb1[, 2] - s[2]) ^ 2 +
+                (vb1[, 3] - s[3]) ^ 2)
+  vb2_d <- sqrt((vb2[, 1] - s[1]) ^ 2 + (vb2[, 2] - s[2]) ^ 2 +
+                (vb2[, 3] - s[3]) ^ 2)
 
   # Determine intersecting vertices (i.e. one end in, one end outside sphere)
-  eoi.a <- cbind(vb1.d >= d, vb2.d < d) # Edges of interest (aux tmp)
-  eoi.b <- cbind(vb1.d < d, vb2.d >= d) # Edges of interest (aux tmp)
+  eoi_a <- cbind(vb1_d >= d, vb2_d < d) # Edges of interest (aux tmp)
+  eoi_b <- cbind(vb1_d < d, vb2_d >= d) # Edges of interest (aux tmp)
   # This is VERY IMPORTANT - We don't know which side is further!
-  eoi <- c(which(eoi.a[, 1] == T & eoi.a[, 2] == T),
-           which(eoi.b[, 1] == T & eoi.b[, 2] == T))
+  eoi <- c(which(eoi_a[, 1] == TRUE & eoi_a[, 2] == TRUE),
+           which(eoi_b[, 1] == TRUE & eoi_b[, 2] == TRUE))
 
-  if (length(eoi) == 0){
-    return(NA)
+  if (length(eoi) == 0) {
+    return(empty_out)
   }
 
   # Get intersection points:
-  l.i <- list()
+  res <- list()
   for (i in seq_along(eoi)){
-    l.s <- rbind(vb1[eoi[i], ], vb2[eoi[i], ])
-    l.res <- l2sIntersect(l.s, s)
-    p.d <- coords_onseg(l.res, l.s, tol = 0.001)
-    
-    if (sum(p.d) == 0){
-     stop("No intersections found. This should never happen here.") 
+    l_seg <- rbind(vb1[eoi[i], ], vb2[eoi[i], ])
+    l_int <- l2sIntersect(l_seg, s)
+    p_d <- coords_onseg(l_int, l_seg, tol = 0.001)
+
+    if (sum(p_d) == 0) {
+      stop("No intersections found. This should never happen here.")
     }
 
-    l.i[[i]] <- l.res[p.d, ]
+    res[[i]] <- l_int[p_d, ]
   }
 
-  res <- data.frame(do.call("rbind", l.i))
-  colnames(res) <- c("x", "y", "z")
+  res <- data.frame(do.call("rbind", res))
+  colnames(res) <- out_colnames
   rownames(res) <- eoi
   return(res)
 }
