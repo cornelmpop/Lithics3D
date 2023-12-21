64a2bc07460d26f335a6845243840e1bc092e316
Cornel M. Pop <pop.cornelm@gmail.com>
2023-12-21 13:27:31 -0800

    Added first draft of a new function: edge_angles_vis2d

--- R/edgeAngles_visualize.R
+++ R/edgeAngles_visualize.R
@@ -68,7 +68,7 @@ edge_angles_vis3d <- function(ea_output, show_mesh = NULL, show_pois = NULL) {
     stop("A non-mesh3d object supplied via the show_mesh parameter")
   }
   if (!is.null(show_pois) &&
-      (is.null(dim(show_pois)) || !dim(show_pois)[2] == 3)) {
+        (is.null(dim(show_pois)) || !dim(show_pois)[2] == 3)) {
     stop("Incorrect dimensions for show_pois")
   }
 
@@ -103,4 +103,123 @@ edge_angles_vis3d <- function(ea_output, show_mesh = NULL, show_pois = NULL) {
 
   # Return the RGL window ID
   return(cw)
+}
+
+
+#' 2D visualizer for edgeAngles output
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' Shows how edge angles were measured in a previous run of the [edgeAngles()]
+#' function.
+#'
+#' @details
+#' Loops through the output of [edgeAngles()] and plots key parameters for each
+#' angle measurement. The number of sub-plots per figure will be computed based
+#' on the `ncol` and `nrow` parameters. If you would like individual figures
+#' for each sub-plot, simply set both `ncol` and `nrow` to 1.
+#'
+#' Note that all sub-plots from given output figure will be drawn to the same
+#' scale, but different figures will have different scales.
+#'
+#' @param ea_output A list corresponding to the unmodified output of
+#' [edgeAngles()].
+#'
+#' @param mesh An `mesh3d` object corresponding to the mesh on which the angles
+#' were measured.
+#'
+#' @param ncol A number indicating the number of sub-plots to fit horizontally
+#' in each output figure.
+#'
+#' @param nrow A number indicating the number of sub-plots to fit vertically
+#' in each output figure.
+#'
+#' @return A list of figures containing subplots for each angle measurement;
+#' each is labelled according to the index of the measurement along the edge.
+#'
+#' @author Cornel M. Pop
+#'
+#' @examples
+#' \dontrun{
+#' # Compute edge angles using included demo data:
+#' data(demoFlake2)
+#' e.curve = sPathConnect(demoFlake2$lms[1:4, ],
+#'                        demoFlake2$mesh, path.choice = "ridges")
+#' mv <- t(demoFlake2$mesh$vb)
+#' path.res <- pathResample(mv[e.curve,1:3], 30, method = "npts")
+#' res = edgeAngles(demoFlake2$mesh, path.res, m.d = 3)
+#'
+#' # Generate figures:
+#' angle_plots <- edge_angles_vis2d(res,
+#'                                  mesh = demoFlake2$mesh,
+#'                                  ncol = 3)
+#' # See the first figure:
+#' angle_plots[[1]]
+#'}
+#' @export
+edge_angles_vis2d <- function(ea_output, mesh, ncol = 2, nrow = 2) {
+
+  # Prep mesh data:
+  mvb <- t(mesh$vb)
+  medges <- Rvcg::vcgGetEdge(mesh, unique = TRUE)
+
+  # Loop through the results:
+  out_res <- list()
+  for (i in seq_along(ea_output)) {
+    # plane coords - NOTE: Since inters.pts are approximated, this is NOT
+    # the same as the curve.pp output!
+    tcoords <- rbind(ea_output[[i]]$lm[3, ],
+                     ea_output[[i]]$inters.pts[1:2, ])
+
+    # Align the mesh with the axes for useful plot:
+    # TODO: It probably makes more sense to only translate the coords of the
+    # edges we are going to plot, instead of the whole mesh. Pre-process?
+    res_o <- orient_by_vectors(rbind(tcoords[1, ], colMeans(tcoords[2:3, ])),
+                               tcoords[c(1, 3), ],
+                               mvb[, 1:3], e_coords = tcoords)
+
+    eoi <- edgesOnPlane(rbind(ea_output[[i]]$lm[3, ],
+                              ea_output[[i]]$inters.pts[1:2, ]),
+                        mvb, medges)
+
+    # Extract coords to plot:
+    # Coords of first edge end
+    vb1 <- res_o$coords[medges[eoi, 1], 1:3, drop = FALSE]
+    # Coords of second edge end
+    vb2 <- res_o$coords[medges[eoi, 2], 1:3, drop = FALSE]
+
+    endpts <- data.table(rbind(vb1, vb2))
+    names(endpts) <- c("x", "y", "z")
+    endpts$geom <- "pts"
+    markerpts <- data.table(res_o$e_coords[c(3, 1, 2), ])
+    names(markerpts) <- c("x", "y", "z")
+    markerpts$geom <- "lines"
+
+    out_res[[i]] <- rbind(endpts, markerpts)
+    out_res[[i]]$idx <- i
+  }
+
+  # Group plots according to the requested ncol/nrow:
+  plot_group_f <- cut(seq_along(ea_output),
+                      ceiling(length(ea_output) / (ncol * nrow)),
+                      labels = FALSE)
+  plot_groups <- split(out_res, plot_group_f)
+
+  out_plots <- list()
+  for (i in seq_along(plot_groups)) {
+    res_sub <- rbindlist(plot_groups[[i]])
+
+    out_plots[[i]] <- ggplot(res_sub, aes(x, y)) +
+      coord_fixed() + theme_bw() + labs(x = "", y = "") +
+      geom_point(data = res_sub[geom == "pts"],
+                 shape = 20, size = 1) +
+      geom_path(data = res_sub[geom == "lines"],
+                color = "red", alpha = 0.6, lwd = 2) +
+      geom_point(data = res_sub[geom == "lines"],
+                 color = "red", size = 2) +
+      facet_wrap(~idx, ncol = ncol)
+  }
+
+  return(out_plots)
 }
