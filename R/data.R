6a27f3478e6240643edd5f1a544f5f92fa92da6f
cornelmpop <pop.cornelm@gmail.com>
2024-02-08 12:04:29 -0800

    Removed demoFlake3. The mesh was not used by any code in the package, and it was pushing the distribution size above acceptable CRAN limits.

--- R/data.R
+++ R/data.R
@@ -24,19 +24,6 @@
 #' LTD <http://www.popgeology.com>
 "demoFlake2"
 
-#' @title Flake 3
-#' @description A demo flake (PGCS_RI.1) for use with the Lithics3D package.
-#' @author Cornel M. Pop
-#' @docType data
-#' @keywords datasets
-#' @name demoFlake3
-#' @usage data(demoFlake3)
-#' @format A list containing a mesh3d object (mesh) and a set of basic landmarks
-#' (lms) defining the ventral surface of the flake
-#' @source 3D scan courtesy of Pop Geological Consulting Services
-#' LTD <http://www.popgeology.com>
-"demoFlake3"
-
 #' @title Demo sphere
 #' @description A small closed sphere used to test functions within this package
 #' @docType data
