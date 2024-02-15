837bada0192bbbe6096a6e3d8c8a8ebf370add1c
cornelmpop <pop.cornelm@gmail.com>
2024-02-14 22:34:30 -0800

    mesh_segment_by_path: Changed the algorithm to re-add orphaned faces (see issue #23). Also cleaned the code a bit, made some minor changes to the function's documentation, and added tests.

--- R/mesh_splitbypath.R
+++ R/mesh_splitbypath.R
@@ -4,33 +4,36 @@
 #' @description Cuts a mesh along a closed path defined on the basis of an
 #' ordered set of coordinates (ideally landmarks), and splits the mesh into
 #' multiple components. Note that you must provide a clean mesh, so please use
-#' [vcgClean] on the input.
+#' [Rvcg::vcgClean()] on the input.
 #' @author Cornel M. Pop
-#' @param mesh A (clean) mesh3d object
-#' @param lms (Deprecated) An ordered set of coordinates. Since this parameter
-#' is being deprecated, please use the output of [sPathConnect] to the
-#' mesh.path argument)
-#' @param path.choice (Deprecated) Described in [sPathConnect].
+#' @param mesh A (clean) `mesh3d` object
+#' @param lms (*Deprecated*) An ordered set of coordinates. Since this parameter
+#' is being deprecated, please use the output of [sPathConnect()] to the
+#' `mesh.path` argument)
+#' @param path.choice (*Deprecated*) Described in [sPathConnect()].
 #' @param mesh.path A vector containing a list of connected vertices forming a
 #' closed path on the surface of the input mesh. This parameter should
-#' correspond to the output of [sPathConnect]
+#' correspond to the output of [sPathConnect()]
 #' @return A list of mesh objects containing a minimum of 3 faces each.
 #' @examples
 #' library(rgl)
-#' mesh.path <- sPathConnect(demoFlake2$lms, demoFlake2$mesh,
-#'                           path.choice="ridges", closed=TRUE)
-#' mesh.segs <- mesh_segment_by_path(demoFlake2$mesh, mesh.path=mesh.path)
-#' shade3d(mesh.segs[[1]], color="green")
-#' shade3d(mesh.segs[[2]], color="blue")
-#' @note Minimum output mesh segment size is 3 faces. Anything smaller gets
-#' discarded silently.
-#' 
+#' mesh_path <- sPathConnect(demoFlake2$lms, demoFlake2$mesh,
+#'                           path.choice = "ridges", closed = TRUE)
+#' mesh_segs <- mesh_segment_by_path(demoFlake2$mesh, mesh.path = mesh_path)
+#' \dontrun{
+#' shade3d(mesh_segs[[1]], color = "green")
+#' shade3d(mesh_segs[[2]], color = "blue")
+#' close3d()
+#' }
+#' @note Minimum output mesh segment size is 3 faces, **not including faces
+#' along the mesh border**. Anything smaller gets discarded silently.
+#'
 #' Note also that the deprecated parameters will stop working in version 1 of
 #' this package at the latest.
 #' @export
 mesh_segment_by_path <- function(mesh, lms = NULL, path.choice = "ridges",
-                                 mesh.path = NULL){
-  mesh.it <- t(mesh$it)
+                                  mesh.path = NULL) {
+  mesh_it <- t(mesh$it)
 
   # For compatibility purposes:
   if (!is.null(nrow(lms))) {
@@ -39,36 +42,75 @@ mesh_segment_by_path <- function(mesh, lms = NULL, path.choice = "ridges",
             call. = TRUE)
   }
 
-  # Remove triangles along the path - TODO: Clean.
-  path.it <- c(which(mesh.it[, 1] %in% mesh.path),
-               which(mesh.it[, 2] %in% mesh.path),
-               which(mesh.it[, 3] %in% mesh.path))
+  # Identify faces/triangles along the path:
+  # NOTE: This should be the same as getFaces(mesh, mesh.path), except that
+  # the output is not sorted as in the latter.
+  path_it <- c(which(mesh_it[, 1] %in% mesh.path),
+               which(mesh_it[, 2] %in% mesh.path),
+               which(mesh_it[, 3] %in% mesh.path))
+  path_it <- unique(path_it)
 
   # Create a new mesh without the selected triangles:
-  mesh.new <- list(vb = mesh$vb, it = t(mesh.it[-path.it, ]))
-  class(mesh.new) <- "mesh3d"
+  mesh_new <- list(vb = mesh$vb, it = t(mesh_it[-path_it, ]))
+  class(mesh_new) <- "mesh3d"
 
   # Split cut mesh into multiple segments
-  mesh.segs <- vcgIsolated(mesh.new, split = T)
+  mesh_segs <- vcgIsolated(mesh_new, split = TRUE)
 
-  # Add back the removed triangles. TODO: There must be a better way of
+  # Check for orphaned faces/triangles:
+  orig_vb <- list()
+  for (i in seq_along(mesh_segs)) {
+    # Map vertex ids of new mesh onto original:
+    orig_vb[[i]] <- mapOnMesh(data.frame(t(mesh_segs[[i]]$vb)), mesh)$vertex
+  }
+  # Identify dropped vertices:
+  dropped_v <- setdiff(1:ncol(mesh$vb), unique(unlist(orig_vb)))
+  # Identify orphan faces/triangles
+  orphan_f <- which(mesh$it[1, ] %in% dropped_v &
+                      mesh$it[2, ] %in% dropped_v &
+                      mesh$it[3, ] %in% dropped_v)
+  # Identify faces adjacent (two shared vertices) to orphan(s):
+  orphan_f_v <- mesh$it[, orphan_f, drop = FALSE] # vertices from orphan faces
+
+  # Get connected faces:
+  # Start with all faces sharing a vertex with an orphan:
+  adj_f <- getFaces(mesh, as.vector(orphan_f_v))
+  adj_f_v <- mesh$it[, adj_f, drop = FALSE] # Vertex IDs for the adjacent faces
+
+  # Check how many vertices are shared by the identified adjacent faces (sums
+  # TRUE, which translates to a numeric value of 1).
+  orphans <- list()
+  for (i in seq_along(orphan_f)) {
+    adj_f_vr <- adj_f_v[1, ] %in% orphan_f_v[, i] +
+                adj_f_v[2, ] %in% orphan_f_v[, i] +
+                adj_f_v[3, ] %in% orphan_f_v[, i]
+    adj_fo <- adj_f[which(adj_f_vr == 2)] # Filter: keep faces with 2 shared v.
+    orphans[[i]] <- data.frame(o = orphan_f[i], adj = adj_fo)
+  }
+  orphans <- rbindlist(orphans)
+
+  # Add back the removed triangles. NOTE: There must be a better way of
   # doing this?
   mesh_segs_full <- list()
-  i <- 1
-  for (ms in mesh.segs){
-    print(nrow(ms$vb))
-    # Map vertex ids of new mesh onto original:
-    orig.vb <- mapOnMesh(data.frame(t(ms$vb)), mesh)$vertex
+  for (i in seq_along(mesh_segs)){
     # Get faces associated with this segment:
-    seg.it <- Morpho::getFaces(mesh, orig.vb)
-    if (length(seg.it) > 3){
-      mesh_segs_full[[i]] <- list(vb = mesh$vb, it = mesh$it[, seg.it])
+    seg_it <- Morpho::getFaces(mesh, orig_vb[[i]])
+
+    # Does this mesh have faces adjacent to any orphans? If so, add connected
+    # orphans:
+    o_idx <- which(orphans$adj %in% intersect(unique(orphans$adj), seg_it))
+    o_readded <- orphans[o_idx, ]$o
+    seg_it <- c(seg_it, o_readded)
+    orphans <- orphans[!orphans$o %in% o_readded]
+
+    if (length(seg_it) > 3) {
+      mesh_segs_full[[i]] <- list(vb = mesh$vb, it = mesh$it[, seg_it])
 
       class(mesh_segs_full[[i]]) <- "mesh3d"
       mesh_segs_full[[i]] <- Morpho::rmUnrefVertex(mesh_segs_full[[i]],
-                                                   silent = F)
-      i <- i + 1
+                                                   silent = FALSE)
     }
   }
+
   return(mesh_segs_full)
 }
--- tests/testthat/test-mesh_segment_by_path.R
+++ tests/testthat/test-mesh_segment_by_path.R
@@ -4,36 +4,32 @@ test_that("mesh_segment_by_path works as expected", {
   mesh <- demoSphere
   pois <- t(demoSphere$vb)[c(1, 20, 30, 45), 1:3]
   mesh_path <- sPathConnect(pois, mesh, closed = TRUE)
-
   res <- mesh_segment_by_path(mesh, mesh.path = mesh_path)
 
-  # NOTE: This is NOT what the return should be.
   expect_equal(ncol(res[[1]]$vb), 52)
-  expect_equal(ncol(res[[1]]$it), 91)
+  expect_equal(ncol(res[[1]]$it), 93)
   expect_equal(ncol(res[[2]]$vb), 16)
   expect_equal(ncol(res[[2]]$it), 20)
   
-  # TODO!
-  skip("TODO")
-  # Issues: (note - unclear why the first test is off by so much)
-  expect_identical(ncol(res[[1]]$vb) + ncol(res[[2]]$vb),
-                   ncol(mesh$vb))
-  expect_identical(ncol(res[[1]]$it) + ncol(res[[2]]$it),
-                   ncol(mesh$it))
-
-
-  #shade3d(mesh, col="green", alpha = 0.5)
-  #points3d(pois, col="red")
-  #zz <- t(mesh$vb)[mesh_path, ]
-  #lines3d(zz, col="blue")
-  #
-  #shade3d(res[[1]], color="green")
-  #shade3d(res[[2]], color="blue")
+  # Resulting mesh was not watertight prior to the changes in v0.5.3.
+  pois <- t(demoSphere$vb)[c(1, 20, 40, 50), 1:3]
+  mesh_path <- sPathConnect(pois, mesh, closed = TRUE)
+  res <- mesh_segment_by_path(mesh, mesh.path = mesh_path)
   
-  # TODO - issues to fix:
-  pois <- t(demoSphere$vb)[c(1, 20, 40), 1:3] # Overlap in path - only one obj. segmented
-  pois <- t(demoSphere$vb)[c(1, 20), 1:3] # Overlap in path
-  pois <- t(demoSphere$vb)[c(1, 20, 40, 50), 1:3] # Resulting mesh is not air tight.
+  expect_equal(length(res), 2)
+  expect_equal(ncol(res[[1]]$vb), 50)
+  expect_equal(ncol(res[[1]]$it), 88)
+  expect_equal(ncol(res[[2]]$vb), 19)
+  expect_equal(ncol(res[[2]]$it), 25)
   
+  skip_on_cran()
+  mesh_path <- sPathConnect(demoFlake2$lms, demoFlake2$mesh,
+                            path.choice = "ridges", closed = TRUE)
+  mesh_segs <- mesh_segment_by_path(demoFlake2$mesh, mesh.path = mesh_path)
+  
+  expect_equal(length(mesh_segs), 2)
+  expect_equal(ncol(mesh_segs[[1]]$it), 100703)
+  expect_equal(ncol(mesh_segs[[1]]$vb), 50736)
+  expect_equal(ncol(mesh_segs[[2]]$it), 72112)
+  expect_equal(ncol(mesh_segs[[2]]$vb), 36441)
 })
-
